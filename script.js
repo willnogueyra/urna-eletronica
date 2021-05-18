6b7b3d361a79ecaf051247df359e1aa089afa147
William Nogueira <will.ribeiro@live.com>
2021-05-18 17:33:40 -0300

    json criado e elementos com javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.divisao-1-left-1 span');
+let cargo = document.querySelector('.divisao-1-left-2 span');
+let numeros = document.querySelector('.divisao-1-left-3');
+let descricao = document.querySelector('.divisao-1-left-4');
+let lateral = document.querySelector('.divisao-1-right');
+let aviso = document.querySelector('.divisao-2');
